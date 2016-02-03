8578b8f86737e1185f3218ad7c372c13961c01bf
Aleš Jiránek <a.jiranek@gmail.com>
2016-02-03 10:15:40 +0100

    Fix webpack dev server port

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack-dev-server/client?http://0.0.0.0:8080',
     'webpack/hot/only-dev-server',
     './example/index.js'
   ],
